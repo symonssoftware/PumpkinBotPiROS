aa6b63351531501b7c5a3cf25dd88f6a3c483fa0
Ken Symons <ksymons13@gmail.com>
2022-09-18 17:35:20 -0400

    Adding USB camera stream to Pi1 launch script

--- scripts/pumpkin_bot_1_launch.sh
+++ scripts/pumpkin_bot_1_launch.sh
@@ -4,6 +4,7 @@ source /opt/ros/humble/setup.bash
 source ~/ros2_ws/install/setup.bash
 ros2 launch pumpkin_bot_bringup pumpkin_bot_launch.py
 ros2 run pumpkin_py_pkg robot_ui
+ros2 run image_tools cam2image --ros-args --param width:=640 --param height:=480
 
 # exit gracefully by returning a status 
 exit 0
